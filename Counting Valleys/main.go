38cd5b0813b190af1c900a2ba7203d03ffd3347b
Leo Borquez <15177945+LeoBorquez@users.noreply.github.com>
2021-07-08 17:08:59 -0400

    📦 NEW: day1

---
+++ Counting Valleys/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(CountingValleys(12, "DDUUDDUDUUUD"))
+}
+
+func CountingValleys(steps int32, path string) int32 {
+	var l, c int32 = 0, 0
+	for _, val := range strings.Split(path, "") {
+		if val == "U" {
+			if l < 0 && l+1 == 0 {
+				c++
+			}
+			l++
+		} else if val == "D" {
+			l--
+		}
+	}
+	return c
+}
