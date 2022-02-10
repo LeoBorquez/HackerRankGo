a8b3e66a10e8370a29b2aa49942c73c5c4d11d66
German Borquez <gborquez@apple.com>
2022-02-10 16:56:47 -0500

    101 added

--- 101/main.go
+++ 101/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
 )
 
 func main() {
@@ -39,11 +40,43 @@ func getPointer() (myPointer *int) { // return/read a pointer *int
 }
 
 func flowControl() float64 {
+	// statement if
 	lim := 10.5
 	if v := math.Pow(3, 2); v < lim {
 		return v
 	} else {
 		fmt.Println("return 10")
 	}
+
+	// for loop
+	for count := 0; count <= 10; count++ {
+		fmt.Println("My counter is at", count)
+	}
+
+	// for range loop
+	entry := []string{"Jack", "John", "Jones"}
+	for i, val := range entry {
+		fmt.Printf("At position %d, the character %s is present\n", i, val)
+	}
+
+	// while loop
+	n := 0
+	x := 42
+	for n != x {
+		n++
+	}
+
+	// switch
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
 	return lim
 }
